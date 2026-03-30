3e0533738e292db89fe7bf1cc5785c79f2be29b2
arseniyshalamov <arsenshalamov16@gmail.com>
2026-03-30 15:27:48 +0300

    Initial working SAM3 on Mac (CPU fixes)

---
+++ test_sam3_hf.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import torch
+from sam3.model_builder import build_sam3_image_model
+from sam3.model.sam3_image_processor import Sam3Processor
+
+device = "cpu"
+
+print("Loading model...")
+model = build_sam3_image_model()
+model = model.to(device)
+model = model.float()
+model.eval()
+
+processor = Sam3Processor(model, device=device)
+
+print("Loading image...")
+image = Image.open("test.jpg").convert("RGB")
+
+with torch.autocast(device_type="cpu", enabled=False):
+    state = processor.set_image(image)
+
+    print("Running prompt...")
+    output = processor.set_text_prompt(
+        prompt="a dog",
+        state=state,
+    )
+
+print("Done!")
+print(output.keys())
